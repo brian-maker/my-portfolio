263c00c39880b871e3d46b96e6d6649c5307197e
brian <briankipkoech04@gmail.com>
2024-07-14 17:48:17 +0300

    porftolio projects

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -91,7 +91,7 @@ function Portfolio() {
         </div>
         {/* project 3 */}
         <div className="p-4 bg-white shadow-lg shadow-gray-500">
-          <h3 className="text-xl font-bold mb-4">Bookmark Website</h3>
+          <h3 className="text-xl font-bold mb-4">Github user search</h3>
           <article className='flex flex-col gap-4'>
             <div 
             data-aos="zoom-in" data-aos-duration="1500"
@@ -99,10 +99,10 @@ function Portfolio() {
               <div
               className='overlay flex gap-8'>
               <a
-                href="https://github.com/brian-maker/Bookmark-landing-page">
+                href="https://github.com/brian-maker/github-user-search.git">
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24"><path fill="#fff" d="M12.5 0C5.594 0 0 5.51 0 12.305c0 5.437 3.581 10.048 8.547 11.674.625.116.854-.265.854-.592 0-.292-.01-1.066-.016-2.092-3.477.742-4.21-1.65-4.21-1.65-.569-1.42-1.39-1.8-1.39-1.8-1.133-.764.087-.748.087-.748 1.255.086 1.914 1.268 1.914 1.268 1.115 1.881 2.927 1.338 3.641 1.024.113-.797.434-1.338.792-1.646-2.776-.308-5.694-1.366-5.694-6.08 0-1.343.484-2.44 1.286-3.302-.14-.31-.562-1.562.11-3.256 0 0 1.047-.33 3.437 1.261 1-.273 2.063-.409 3.125-.415 1.063.006 2.125.142 3.125.415 2.375-1.591 3.422-1.261 3.422-1.261.672 1.694.25 2.945.125 3.256.797.861 1.281 1.959 1.281 3.302 0 4.727-2.921 5.767-5.703 6.07.438.369.844 1.123.844 2.276 0 1.647-.016 2.97-.016 3.37 0 .322.22.707.86.584 5-1.615 8.579-6.23 8.579-11.658C25 5.509 19.403 0 12.5 0z"/></svg>
               </a>
-              <a href="https://bookmark-landing-page-brian.netlify.app/">
+              <a href="https://github-user-search04.netlify.app/">
               <svg xmlns="http://www.w3.org/2000/svg"
               width="25" height="24"
               viewBox="0 0 512 512"><path
@@ -114,7 +114,8 @@ function Portfolio() {
             <p
             data-aos="zoom-in" data-aos-duration="1500"
             data-aos-delay="100"
-            className="text-[0.9375rem] lg:text-base text-gray-800 font-mono font-semibold">The Bookmark website is a project that showcases a modern and clean design for a bookmarking application. It demonstrates how to build a responsive and interactive website that allows users to save and organize their favorite web pages. </p>
+            className="text-[0.9375rem] lg:text-base text-gray-800 font-mono font-semibold">The github user search website is a project that allows the search of a username using the Github Api. It allows the Search for GitHub users by their username displaying relevant user information based on their search.It also has the option to switch between light and dark themes
+          </p>
           </article>
         </div>
         {/* project 4 */}
